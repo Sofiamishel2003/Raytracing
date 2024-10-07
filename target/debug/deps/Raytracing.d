e07b1e457eec658633bccf31021cf752c4d34bfc
sofiaWebT <98661058+Sofiamishel2003@users.noreply.github.com>
2024-10-07 17:47:55 -0600

    create cargo proyect

---
+++ target/debug/deps/Raytracing.d
@@ -0,0 +1,5 @@
+C:\Users\50250\Desktop\Sofía Mishell Velásquez UVG\Tercer Año 2024\Segundo Semestre\Graficas\Raytracing\target\debug\deps\Raytracing.exe: src/main.rs
+
+C:\Users\50250\Desktop\Sofía Mishell Velásquez UVG\Tercer Año 2024\Segundo Semestre\Graficas\Raytracing\target\debug\deps\Raytracing.d: src/main.rs
+
+src/main.rs:
